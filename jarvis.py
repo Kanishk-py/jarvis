a7b030857e9136dd337159969be5d7ba90227674
Kanishk-py <75932955+Kanishk-py@users.noreply.github.com>
2020-12-28 19:19:50 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -9,13 +9,9 @@ screen = pygame.display.set_mode((800,600))
 is_running=True
 while is_running:
 	screen.blit(pygame.image.load('download.jpeg'),(250,300))
-	screen.blit( font.render("HEHEHEHE ",True,(255, 0, 0)), (350,100))
-	screen.blit( font.render("KARO DEBUG ",True,(255, 0, 0)), (350,120))
-	screen.blit( font.render("AND FOR CLASSIC RICKROLL I HAVE NOT ADDED QUIT OPTION SO...(TASK MANAGER) ",True,(255, 0, 0)), (50,140))
-	screen.blit( font.render("NEVER GONNA GIVE UP RICK ROLLING ITS TOO MUCH FUN ",True,(255, 0, 0)), (100,160))
-	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				is_over = True
-				is_running = False
+# 	for event in pygame.event.get():
+# 			if event.type == pygame.QUIT:
+# 				is_over = True
+# 				is_running = False
 
 	pygame.display.update()
